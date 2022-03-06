5e1adc513712fc1b75689cf85c8d711d0cf2cff6
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-06 08:16:50 +0200

    feat:add import all

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 # print(mod.name)
 # print(mod.number) 
 
-from mod import number, name
+# from mod import number, name
+# print(name)
+# print(number)
+
+from mod import *
 print(name)
 print(number)
