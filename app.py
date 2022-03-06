030cf5049936ca2e8633d7674bf24cf06ac27312
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-06 08:27:04 +0200

    init:start project

--- app.py
+++ app.py
@@ -1,16 +1,1 @@
-# import mod
-# print(mod.name)
-# print(mod.number) 
 
-# from mod import number, name
-# print(name)
-# print(number)
-
-# from mod import *
-# print(name)
-# print(number)
-
-
-import mod
-print(dir())    #command assistants
-print(help(mod)) #command assistants
--- mod.py
+++
@@ -1,2 +0,0 @@
-name='Ray'
-number=5
